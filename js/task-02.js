883d1e467bc61ea0bec4b3138228d9f31b4406b8
Yaroslav Semenov <yaryk2013@gmail.com>
2021-12-19 17:09:34 +0200

    completed task 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const addIngregirntItemEl = ingredients.map((ingredient) => {
+  let ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.textContent = ingredient;
+  ingredientsItemEl.classList.add("item");
+
+  return ingredientsItemEl;
+});
+
+const ingredientsListEl = document.querySelector("#ingredients");
+ingredientsListEl.append(...addIngregirntItemEl);
