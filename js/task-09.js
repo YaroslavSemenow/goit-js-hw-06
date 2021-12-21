5f4be9ac302c11ab0acf195a9b231e445072e901
Yaroslav Semenov <yaryk2013@gmail.com>
2021-12-22 00:59:07 +0200

    completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  body.style.backgroundColor = getRandomHexColor();
+
+  colorValue.textContent = getRandomHexColor();
+}
